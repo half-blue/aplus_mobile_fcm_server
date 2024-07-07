036f0f1ca2b413c794bcdf468c21249102a68a7b
Takumi Tamura <tamutamuphone@gmail.com>
2024-07-07 17:53:50 +0900

    モデル変更・現在の学群を表示

--- app/models.py
+++ app/models.py
@@ -6,6 +6,19 @@ from fcm_django.models import FCMDevice
 class Subscription(models.Model):
     device = models.OneToOneField(FCMDevice, blank=False, primary_key=True, on_delete=models.CASCADE)
     threads = models.ManyToManyField('Thread', blank=True)
+    GAKUGUNS = (
+        (0,  '未設定'),
+        (1, '人文・文化学群'),
+        (2, '社会．国際学群'),
+        (3, '人間学群'),
+        (4, '生命環境学群'),
+        (5, '理工学群'),
+        (6, '情報学群'),
+        (7, '医学群'),
+        (8, '体育専門学群'),
+        (9, '芸術専門学群'),
+    )
+    affiliation = models.IntegerField(choices=GAKUGUNS, verbose_name='所属学群', default=0)
 
     def __str__(self):
         return self.device.registration_id
--- app/views.py
+++ app/views.py
@@ -41,12 +41,13 @@ class NoticeManegeIndexView(TemplateView):
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
+
+        def get_gakugun_name(gakugun):
+            return Subscription.GAKUGUNS[gakugun][1]
+
         context = super().get_context_data(**kwargs)
         fcm_token = self.request.COOKIES.get('fcm_token')
         subscription = Subscription.objects.get(device__registration_id = fcm_token) # 存在することが保証される
-        # ここから先は取り急ぎ実装です
-        threads = subscription.threads.all()
-        thread_ids = [thread.thread_id for thread in threads]
-        context['threads'] = thread_ids
+        context['gakugun'] = get_gakugun_name(subscription.affiliation)
         return context
     
