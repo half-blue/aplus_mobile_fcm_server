cf613df76a32553f001ef015ff40751fd8dd0569
nitoca <78777225+nitoca@users.noreply.github.com>
2024-09-22 23:29:19 +0900

    コンマ修正とスペースを追加

--- app/models.py
+++ app/models.py
@@ -7,16 +7,16 @@ class Subscription(models.Model):
     device = models.OneToOneField(FCMDevice, blank=False, primary_key=True, on_delete=models.CASCADE)
     threads = models.ManyToManyField('Thread', blank=True)
     GAKUGUNS = (
-        (0,  '未設定'),
-        (1, '人文・文化学群(A)'),
-        (2, '社会．国際学群(B)'),
-        (3, '人間学群(C)'),
-        (4, '生命環境学群(E)'),
-        (5, '理工学群(F)'),
-        (6, '情報学群(G)'),
-        (7, '医学群(H)'),
-        (8, '体育専門学群(W)'),
-        (9, '芸術専門学群(Y)'),
+        (0, '未設定'),
+        (1, '人文・文化学群 (A)'),
+        (2, '社会・国際学群 (B)'),
+        (3, '人間学群 (C)'),
+        (4, '生命環境学群 (E)'),
+        (5, '理工学群 (F)'),
+        (6, '情報学群 (G)'),
+        (7, '医学群 (H)'),
+        (8, '体育専門学群 (W)'),
+        (9, '芸術専門学群 (Y)'),
     )
     affiliation = models.IntegerField(choices=GAKUGUNS, verbose_name='所属学群', default=0)
 
